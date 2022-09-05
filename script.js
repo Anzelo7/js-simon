fe7d352fe3cd3abcc5a3fd56fc22e8ed38b2eac6
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-09-05 15:28:25 +0200

    push

--- script.js
+++ script.js
@@ -9,20 +9,34 @@ let numbers = document.querySelector('randNumber');
 
 numbers = [];
 
-for (i = 1; i < 6; i++) {
+for (i = 1; i < 5; i++) {
     numbers = Math.floor(Math.random() * 5) + 1;
+    console.log(numbers)
 }
 document.getElementById('randNumber').innerHTML = numbers
 
 //countdown di 30s con successiva richiesta
 let countdown = 30;
 
-const intervalId = setInterval(function(){
-    if (countdown > 0){
-         console.log(countdown);
-    }else{
+const intervalId = setInterval(function () {
+    if (countdown > 0) {
+
+    } else {
         countdown = parseInt(prompt('Inserisci i numeri precedentemente esposti'))
+        console.log(countdown);
     }
     countdown--;
 
 }, 1000)
+
+setTimeout(function () {
+    clearInterval(intervalId);
+}, (countdown * 1000) + 1200)
+
+//individuare numeri esatti
+
+if (numbers === countdown) {
+    console.log(countdown)
+} else {
+    console.log('numeri errati')
+}
